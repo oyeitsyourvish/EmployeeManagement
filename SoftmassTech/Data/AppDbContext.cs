02f5a7e651bdfd87175a67e704f8c14a1a48e3b9
Vishal <chinchanevishal03@gmail.com>
2024-01-16 19:28:39 +0530

    Till Created DbContext Class

---
+++ SoftmassTech/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftmassTech.Models;
+
+namespace SoftmassTech.Data
+{
+	public class AppDbContext : DbContext
+	{
+
+		//Constructor.
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {
+
+		}
+
+		//DbSet Properties.
+		public DbSet<Department> Departments { get; set; }
+		public DbSet<Employee> Employees { get; set; }
+	}
+}
