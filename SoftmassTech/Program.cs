0523f127460de20246abaea598e6d642f941bbcc
Vishal <chinchanevishal03@gmail.com>
2024-01-17 16:30:00 +0530

    Moved repositories folder from Models to SoftTech

--- SoftmassTech/Program.cs
+++ SoftmassTech/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using SoftmassTech.Data;
-using SoftmassTech.Models.Repositories;
+using SoftmassTech.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
---
+++ SoftmassTech/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,14 @@
+﻿using SoftmassTech.Models;
+
+namespace SoftmassTech.Repositories
+{
+    public interface IEmployeeRepository
+    {
+        Task<Employee> GetByIdAsync(int id);
+        Task<List<Employee>> GetAllAsync();
+        Task AddAsync(Employee employee);
+        Task UpdateAsync(Employee employee);
+        Task DeleteAsync(int id);
+
+    }
+}
