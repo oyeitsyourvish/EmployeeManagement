fc9fbe9fd6234a6053a199dbfbe1f6dc9cd3f3c7
Vishal <chinchanevishal03@gmail.com>
2024-01-18 15:02:50 +0530

    Add-Migration Cmd will  be succeded

--- SoftmassTech/obj/Debug/net8.0/SoftmassTech.AssemblyInfo.cs
+++ SoftmassTech/obj/Debug/net8.0/SoftmassTech.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoftmassTech")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7391d8bee67e9df3c99c0e3ccd8c93c84db9720d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoftmassTech")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoftmassTech")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
