3183145aa3c620f8e4166844573bc2babe028f8d
Vishal <chinchanevishal03@gmail.com>
2024-01-18 15:09:20 +0530

    ad

--- SoftmassTech/obj/Debug/net8.0/SoftmassTech.AssemblyInfo.cs
+++ SoftmassTech/obj/Debug/net8.0/SoftmassTech.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoftmassTech")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6451b8b08f4b21219fe26d0997830adcf408e4b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoftmassTech")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoftmassTech")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
