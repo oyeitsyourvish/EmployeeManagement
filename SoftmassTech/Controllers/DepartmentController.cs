53eb596cbe0da5aa7c51805d9b0655f575391e48
Vishal <chinchanevishal03@gmail.com>
2024-01-19 12:27:04 +0530

    Desinign add Employee and Add Department form

---
+++ SoftmassTech/Controllers/DepartmentController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SoftmassTech.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
---
+++ SoftmassTech/Controllers/EmployeeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SoftmassTech.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
