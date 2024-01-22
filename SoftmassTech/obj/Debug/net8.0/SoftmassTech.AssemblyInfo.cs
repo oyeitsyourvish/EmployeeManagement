261fc299c77fa1b81d055087bc006f35fbb748a3
Vishal <chinchanevishal03@gmail.com>
2024-01-22 18:59:02 +0530

    want merging branch14

--- SoftmassTech/obj/Debug/net8.0/SoftmassTech.AssemblyInfo.cs
+++ SoftmassTech/obj/Debug/net8.0/SoftmassTech.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoftmassTech")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f76225ec5e00631dc2c29e89531612848370f73c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ea6962330c87a94ec094d89f5eee886251b734f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoftmassTech")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoftmassTech")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
