b772fd662f93a1afe565d8f6385aace1d00670ee
Vishal <chinchanevishal03@gmail.com>
2024-01-23 19:20:19 +0530

    Search and employee list button designed

--- SoftmassTech/obj/Debug/net8.0/SoftmassTech.AssemblyInfo.cs
+++ SoftmassTech/obj/Debug/net8.0/SoftmassTech.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoftmassTech")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c815a2f38316002a1828a68a52e8907dfcb9865c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da48da6c5265d47381386750100223264319a880")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoftmassTech")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoftmassTech")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
