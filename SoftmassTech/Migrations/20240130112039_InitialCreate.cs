3efceea919e085c7138896e5d0f56944cfad6a53
Vishal <chinchanevishal03@gmail.com>
2024-01-31 11:42:17 +0530

    Just some changes

---
+++ SoftmassTech/Migrations/20240130112039_InitialCreate.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SoftmassTech.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Mobile",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Departments",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Mobile",
+                table: "Employees");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Departments",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
--- SoftmassTech/obj/Debug/net8.0/SoftmassTech.AssemblyInfo.cs
+++ SoftmassTech/obj/Debug/net8.0/SoftmassTech.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoftmassTech")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5971352689efb204c9a4434b4a401d1d79bd48f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e22022b59fe08be9e30f33fcf9cda53f8b07c7b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoftmassTech")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoftmassTech")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
